b788d948767e84eb7abfe4e561da30a0b0a25779
Chandler Craig <hellochandlercraig@gmail.com>
2022-01-11 13:14:50 -0800

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,22 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(tStr) {
+  const fulTime = tStr.split(":")
+  const hr = parseInt(fulTime[0])
+  if (hr < 12) {
+    return "Good Morning"
+  } else if (hr >= 12 && hr <=17) {
+    return "Good Afternoon"
+  } else {
+    return "Good Evening"
+  }
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(msg) {
+  const header = document.getElementById('greeting')
+  header.innerText = msg;
+}
